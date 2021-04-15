964bacf82ff8cb8f2fc5c097fb2a62d596ebab46
baraaAlosaily <seelbaraa@gmail.com>
2021-04-15 19:00:18 +0300

    Fifth problem is solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -104,12 +104,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
+function multiplyArray(testArray) { //eslint-disable-line
+  let sumArr=1;
+  for(let i=0;i<testArray.length;i++){
+  sumArr=sumArr*testArray[i];}
+  let msg='The numbers '+testArray[0]+','+testArray[1]+','+testArray[2]+' have a product of 24.'
+  return[sumArr,msg]
 }
-
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
